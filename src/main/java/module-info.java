5698e1b8756ece62e2c15ea77868f6c837cc7318
Michael-Bissinger <Michael.Bissinger@inb.de>
2021-11-08 15:47:09 +0100

    setup maven

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module Converter {
+//  TODO: Konverter später noch durch sauberes Maven ersetzen
+//  https://openjfx.io/openjfx-docs/#maven
 
-    requires javafx.controls;
     requires javafx.graphics;
+    requires javafx.controls;
+
+//    requires javafx.controls;
+//    requires javafx.graphics;
 
     opens ecommerce.converter;
 }
